79548a07217245b455fb158efea9bcfb39682e45
sinziananicolae <sinzianna.nicolae@gmail.com>
2016-08-13 00:14:09 +0300

    Setup project

---
+++ DAIApplication/DAIApplication/App_Start/WebApiConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace DAIApplication
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            var json = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
+            //json.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
+            //json.SerializerSettings.Converters.Add(new DateTimeConverter());
+            json.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.None;
+
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
